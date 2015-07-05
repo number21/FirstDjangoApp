10e52a0d31f6ee3851747b09cffcee82b65cfaee
Gregory Mazur <mrgmazur@gmail.com>
2015-07-05 12:56:53 +0300

    add dynamic url on html page

--- TestDjango/urls.py
+++ TestDjango/urls.py
@@ -20,7 +20,10 @@ from taskmanager import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.home, name='home'),
+    url(r'^$', views.task_list, name='home'),
+    url(r'^tasks/add$', views.task_add, name='task_add'),
+    url(r'^tasks/(?P<taskid>[0-9]+)/edit$', views.task_edit, name='task_edit'),
+    url(r'^tasks/(?P<taskid>[0-9]+)/delete$', views.task_delete, name='task_delete'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
