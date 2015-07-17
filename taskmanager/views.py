cd7184b915f4afabe28f104bed31c1f275b517f0
Gregory <mrgmazur@gmail.com>
2015-07-17 19:19:09 +0300

    Add Pagination and sorting

--- taskmanager/views.py
+++ taskmanager/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Task
 # Create your views here.
 
 def task_list(request):
-    # TODO : Підключити БД
     tasks = Task.objects.all()
+    paginator = Paginator(tasks, 2) # Show 25 contacts per page
+    order_by = request.GET.get("order_by", "")
+
+    if order_by in ("name", "customer", "date_end"):
+        tasks = tasks.order_by(order_by)
+        if request.GET.get("reverse", "") =="1":
+            tasks = tasks.reverse()
+
+    page = request.GET.get('page')
+    try:
+        tasks = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        tasks = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        tasks = paginator.page(paginator.num_pages)
+
     return render(request, 'task_list.html', {"tasks": tasks})
 
 def task_add(request):
