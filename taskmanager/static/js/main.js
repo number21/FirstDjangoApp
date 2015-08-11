ae35a07b0bf02a6166558e2a658d43bd2456c1e7
Gregory <mrgmazur@gmail.com>
2015-08-11 23:18:33 +0300

    Added calculating elapsed time, added highlighting passed task

--- taskmanager/static/js/main.js
+++ taskmanager/static/js/main.js
@@ -1,33 +1,48 @@
 /**
  * Created by gregory on 8/11/15.
  */
-function diffTime() {
-    var ends = document.getElementsByClassName('end');
-    var elapseds = document.getElementsByClassName('elapsed');
-    for (i = 0; i < ends.length; i++) {
-        var start = new Date();
-        console.log(start);
-        var end = new Date(ends.item(i).innerHTML);
-        console.log(end);
-        var diff = new Date(end.getTime() - start.getTime());
-        var years = diff.getUTCFullYear() - 1970;
-        var months = diff.getUTCMonth() - 1;
-        var days = diff.getUTCDate() - 1;
-        var hours = diff.getUTCHours();
-        var minutes = diff.getUTCMinutes();
-        var seconds = diff.getUTCSeconds();
-        elapseds.item(i).innerHTML = "" + years + "р " + months + "м " + days + "д " + hours + "г " + minutes + "х " + seconds + "с ";
-        t = setTimeout('diffTime()', 500);
-    }
 
-}
 function initDateFields() {
     $('input.dateinput').datetimepicker({
         'format': 'YYYY-MM-DD',
         locale: 'uk'
     });
 }
-$(document).ready(function () {
-    initDateFields();
-    diffTime();
-});
+function diffTime() {
+    var ends = document.getElementsByClassName('end');
+    var elapseds = document.getElementsByClassName('elapsed');
+    var flag = false;
+    for (i = 0; i < ends.length; i++) {
+        var start = new Date();
+        var end = new Date(ends.item(i).innerHTML);
+        var punish = "punished"
+        var diff = new Date(end.getTime() - start.getTime());
+        var years = diff.getUTCFullYear() - 1970;
+        console.log(elapseds)
+        if(years < 0) {
+            elapseds.item(i).innerHTML = "Дедлайн минув";
+            console.log(elapseds.item(i).className.indexOf(punish));
+            if(elapseds.item(i).className.indexOf(punish) < 0)
+            {
+                elapseds.item(i).className = elapseds.item(i).className + " " + punish;
+            }
+
+            continue;
+        }
+        var years = diff.getUTCFullYear() - 1970;
+        var months = diff.getUTCMonth() - 1;
+        var days = diff.getUTCDate() - 1;
+        var hours = diff.getUTCHours();
+        var minutes = diff.getUTCMinutes();
+        var seconds = diff.getUTCSeconds();
+        var strBuilder = years + "р " + months + "м " + days + "д " + hours + "г " + minutes + "х " + seconds + "с ";
+        elapseds.item(i).innerHTML =strBuilder;
+
+    }
+    t = setTimeout('diffTime()', 500);
+}
+
+    $(document).ready(function () {
+        initDateFields();
+        diffTime();
+    });
