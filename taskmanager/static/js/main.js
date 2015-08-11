a490d42ede2ae90900f76125b6f39eeb034e759b
Gregory <mrgmazur@gmail.com>
2015-08-11 23:49:58 +0300

    Added calculating elapsed time, added highlighting passed task

--- taskmanager/static/js/main.js
+++ taskmanager/static/js/main.js
@@ -11,22 +11,29 @@ function initDateFields() {
 function diffTime() {
     var ends = document.getElementsByClassName('end');
     var elapseds = document.getElementsByClassName('elapsed');
+    var statuses = document.getElementsByClassName('status');
     var flag = false;
     for (i = 0; i < ends.length; i++) {
         var start = new Date();
         var end = new Date(ends.item(i).innerHTML);
-        var punish = "punished"
+        var punish = "punished";
+        var rewar = "reward";
         var diff = new Date(end.getTime() - start.getTime());
         var years = diff.getUTCFullYear() - 1970;
-        console.log(elapseds)
-        if(years < 0) {
+        console.log(elapseds);
+        if (years < 0) {
             elapseds.item(i).innerHTML = "Дедлайн минув";
             console.log(elapseds.item(i).className.indexOf(punish));
-            if(elapseds.item(i).className.indexOf(punish) < 0)
-            {
+            if (elapseds.item(i).className.indexOf(punish) < 0) {
                 elapseds.item(i).className = elapseds.item(i).className + " " + punish;
             }
-
+            continue;
+        }
+        if (statuses.item(i).innerHTML == "Готово") {
+            elapseds.item(i).innerHTML = "Well done";
+            if (elapseds.item(i).className.indexOf(rewar) < 0) {
+                elapseds.item(i).className = elapseds.item(i).className + " " + rewar;
+            }
             continue;
         }
         var years = diff.getUTCFullYear() - 1970;
@@ -36,13 +43,13 @@ function diffTime() {
         var minutes = diff.getUTCMinutes();
         var seconds = diff.getUTCSeconds();
         var strBuilder = years + "р " + months + "м " + days + "д " + hours + "г " + minutes + "х " + seconds + "с ";
-        elapseds.item(i).innerHTML =strBuilder;
+        elapseds.item(i).innerHTML = strBuilder;
 
     }
     t = setTimeout('diffTime()', 500);
 }
 
-    $(document).ready(function () {
-        initDateFields();
-        diffTime();
-    });
+$(document).ready(function () {
+    initDateFields();
+    diffTime();
+});
