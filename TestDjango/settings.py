52ddd5017f64b40b7e26ccbe570aeae0910a7ac8
Gregory <mrgmazur@gmail.com>
2015-08-23 21:50:37 +0300

    Deleted duplicated url

--- TestDjango/settings.py
+++ TestDjango/settings.py
@@ -160,7 +160,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '..', 'taskmanager', 'static'),
 )
-DEBUG = False
+DEBUG = True
 
 # ##############################################
 # For work with development/production settings
